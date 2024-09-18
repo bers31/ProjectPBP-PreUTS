29fad3dfc5a8a0874c4da08639b8675e01e0b1f7
Faisal Rizki <faisalrizki172004@gmail.com>
2024-09-18 22:47:33 +0700

    login n dahsboard

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/login_page', function () {
+    return view('login_page');
+});
+
+Route::get('/dashboard_mahasiswa', function () {
+    return view('dashboard_mahasiswa');
+});
