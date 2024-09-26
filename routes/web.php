e07ad229e3af7310ffe6ebfcb851e37d72705297
bers31 <suniabernardo@gmail.com>
2024-09-26 09:20:16 +0700

    welcome page and about

--- routes/web.php
+++ routes/web.php
@@ -6,10 +6,18 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::get('/login_page', function () {
+Route::get('/login', function () {
     return view('login_page');
-});
+})->name('login');
 
 Route::get('/dashboard_mahasiswa', function () {
     return view('dashboard_mahasiswa');
+});
+
+Route::get('/about', function () {
+    return view('about');  // Define the route to the about page
+})->name('about');
+
+Route::get('/check-key', function () {
+    return env('APP_KEY');
 });
