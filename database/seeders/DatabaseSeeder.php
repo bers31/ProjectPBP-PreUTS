1b685b96dcfa3e411c8eb952fc95401bf75680a1
Nashwan Adenaya <75899276+OneRicez@users.noreply.github.com>
2024-11-04 18:22:03 +0700

    ADd mk seeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -13,6 +13,7 @@ class DatabaseSeeder extends Seeder
     {
         $this->call([
             Admin::class,
+            FDSSeeder::class
             // DosenSeeder::class,
             // MahasiswaSeeder::class,
         ]);
